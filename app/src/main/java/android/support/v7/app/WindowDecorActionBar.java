090ee9c7f58818409c4311ff8d90187c3f8249ff
abilinski88 <abilinski88@elab-m-02.mchenry.edu>
2016-12-01 16:51:08 -0600

    Medical calc

---
+++ app/src/main/java/android/support/v7/app/WindowDecorActionBar.java
@@ -0,0 +1,14 @@
+package android.support.v7.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by abilinski88 on 12/1/16.
+ */
+public class WindowDecorActionBar extends View {
+    public WindowDecorActionBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
---
+++ app/src/main/java/android/support/v7/widget/ActionBarContainer.java
@@ -0,0 +1,14 @@
+package android.support.v7.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by abilinski88 on 12/1/16.
+ */
+public class ActionBarContainer extends View {
+    public ActionBarContainer(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
